256ad9ebf9ba2a6877fd9f08fdbd5fd50f2e3bc0
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-08-04 09:20:43 -0400

    correct app for form

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
@@ -6,20 +6,40 @@ import ProjectList from "./components/ProjectList";
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [ count, setCount ] = useState(0)
 
-  const handleClick = () => {
+  // console.log("----component rendering----")
+  useEffect(() => {
+    console.log("side effect rendering")
     fetch("http://localhost:4000/projects")
-      .then((response) => response.json())
-      .then((projects) => setProjects(projects));
+      .then(response => response.json())
+      .then(data => setProjects(data))
+  },[])
+
+  // useEffect(() => {
+  //   const timerId = setInterval(() => {
+  //     console.log("timer side effect happening")
+  //     setCount((prevCount) => prevCount + 1);
+  //   }, 1000);
+
+  //   return (() => {
+  //     console.log("cleaning up")
+  //     clearInterval(timerId)
+  //   })
+  // },[count])
+
+  const onAddProject = (newProject) => {
+    setProjects([...projects, newProject]);
   };
+
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm />
-      <button onClick={handleClick}>Load Projects</button>
+      <h1>{count}</h1>
+      <ProjectForm onAddProject= { onAddProject } />
       <ProjectList projects={projects} />
     </div>
   );
