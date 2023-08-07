bfa3fa5b0f923c8d1304467113341bc6a782e394
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-08-07 12:28:25 -0400

    demo done

--- 06_PATCH_DELETE/project_showcase/src/App.js
+++ 06_PATCH_DELETE/project_showcase/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     fetch("http://localhost:4000/projects")
       .then((resp) => resp.json())
       .then((projects) => setProjects(projects));
-  });
+  }, []);
 
   const onToggleDarkMode = () => {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -23,6 +23,41 @@ const App = () => {
   const onAddProject = (newProj) => {
     setProjects((projects) => [...projects, newProj]);
   };
+
+  const onEditProject = (editedProject) => {
+    // console.log("hello from the APP")
+    // console.log(projects)
+    // console.log(editedProject)
+      //1 we need to find the selected project in the state, projects
+      //2 replace that project with the newly edited project
+      //3 update the whole projects using the setter function 
+      const editedProjects = projects.map((project) => {
+        if(project.id === editedProject.id){
+          return editedProject
+        }else{
+          return project
+        }
+      })
+
+      setProjects(editedProjects)
+    
+      console.log(editedProjects)
+  }
+
+  const onDeleteProject = (deletedId) => {
+    console.log(" hello from the app let's delete!")
+    //1 we have to iterate the projects state, get each project
+    //2 if that project's id matches the deletedId, then we want to take that off
+    //and we still need to continue to iterate and finish the iteration
+    //3 we need an array without the matching project
+    //4 use the setter function to update the state
+    const woDeleted = projects.filter((project) => {
+      return project.id !== deletedId
+    })
+    console.log(woDeleted)
+    setProjects(woDeleted)
+
+  }
 
   const completeEditing = () => {
     setProjectId(null);
@@ -38,6 +73,8 @@ const App = () => {
         <ProjectEditForm
           projectId={projectId}
           completeEditing={completeEditing}
+          onEditProject = {onEditProject}
+
         />
       );
     } else {
@@ -52,6 +89,7 @@ const App = () => {
       <ProjectList
         projects={projects}
         enterProjectEditModeFor={enterProjectEditModeFor}
+        onDeleteProject = {onDeleteProject}
       />
     </div>
   );
