2c18f9f3252c3a8b99942a6bae7b100312198229
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-08-23 13:56:31 -0400

    inverse data flow

--- 03_information_flow/project_showcase/src/App.js
+++ 03_information_flow/project_showcase/src/App.js
@@ -6,16 +6,23 @@ import ProjectList from "./components/ProjectList";
 
 const App = () => {
   const [projects, setProjects] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   const handleClick = () => {
     fetch("http://localhost:4000/projects")
       .then((res) => res.json())
-      .then((projects) => setProjects(projects));
+      .then((projects) => {
+        console.log(projects)
+        setProjects(projects)});
   };
 
   return (
-    <div className="App">
-      <Header />
+    <div className={isDarkMode? "App": "App light"}>
+      <Header 
+        isDarkMode={isDarkMode} 
+        onToggleDarkMode={onToggleDarkMode}/>
       <ProjectForm />
       <button onClick={handleClick}>Load Projects</button>
       <ProjectList projects={projects} />
