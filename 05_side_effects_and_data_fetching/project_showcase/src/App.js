33c92551632414062b9f30fb7a0d1ee75510d143
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-08-28 10:56:07 -0400

    useEffect

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 // import ProjectForm from "./components/ProjectForm";
 import ProjectList from "./components/ProjectList";
@@ -8,11 +8,35 @@ const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(true);
   const [ count, setCount ] = useState(0)
 
-  const handleClick = () => {
-    fetch("http://localhost:4000/projects")
+  // const handleClick = () => {
+  //   fetch("http://localhost:4000/projects")
+  //     .then(response => response.json())
+  //     .then(data => setProjects(data))
+  // }
+
+  console.log("-----component rendering -----")
+
+  useEffect(() => { //callback function as the first arg
+      console.log(" side effect rendering ")
+      fetch("http://localhost:4000/projects")
       .then(response => response.json())
-      .then(data => setProjects(data))
-  }
+      .then(projects => setProjects(projects))
+  //}) // no dependency array
+  }, []) // add an empty dependency array as the second argument 
+
+  // useEffect(() => {
+  //   const timerId = setInterval(() => {
+  //     console.log(" timer side effect is happening")
+  //     setCount((prevCount) => prevCount + 1)
+  //   }, 1000)
+
+  //   return (() => {
+  //     console.log("cleaning up")
+  //     clearInterval(timerId)
+  //   })
+  // }, [ count ])
+
+  // console.log(count)
 
   const onAddProject = (newProject) => {
     setProjects([...projects, newProject]);
@@ -24,7 +48,7 @@ const App = () => {
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
       <h1>{count}</h1>
-      <button onClick={handleClick}> Load Projects </button>
+      {/* <button onClick={handleClick}> Load Projects </button> */}
       {/* <ProjectForm onAddProject= { onAddProject } /> */}
       <ProjectList projects={projects} />
     </div>
